55c839ccb7406b21b7098294f43bee571752857f
ahmedfahim21 <ahmed.fahim0207@gmail.com>
2023-01-09 23:10:08 +0530

    error fixed

--- src/main.rs
+++ src/main.rs
@@ -5,9 +5,9 @@ use minigrep::Config;
 fn main() {
     let args: Vec<String> = env::args().collect();
 
-    let config= Config::new(&args).unwrap_or_else(|_err|
+    let config= Config::new(&args).unwrap_or_else(|err|
     {
-        eprintln!("Problem parsing arguments:{}",err);
+        eprintln!("Problem parsing arguments:{err}");
         process::exit(1);}
     );
 
